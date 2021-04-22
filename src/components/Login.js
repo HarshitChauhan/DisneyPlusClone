9d2053c195f65d5d1a239e760b94dd5372721b7c
HarshitChauhan <harshitkumar2503@gmail.com>
2021-04-22 19:28:54 +0530

    Added BackgroundImage and Logo..

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,12 @@ import styled from 'styled-components';
 const Login=(props)=>{
     return (
     <Container>
-        <Content>Content</Content>
+        <Content>
+            <CTA>
+                <CTALogoOne src="/images/cta-logo-one.svg" alt="logo one"/>
+            </CTA>
+            <BgImage/>
+        </Content>
     </Container>
     )
 };
@@ -28,5 +33,30 @@ const Content=styled.div`
     padding: 80px 40px;
     height:100%;
 `;
-
+const BgImage=styled.div`
+    height:100%;
+    background-position:top;
+    background-size:cover;
+    background-repeat:no-repeat;
+    background-image: url("/images/login-background.jpg");
+    position:absolute;
+    top:0;
+    right:0;
+    left:0;
+    z-index:-1;
+`;
+const CTA=styled.div`
+    margin-bottom:2vw;
+    max-width:650px;
+    width:100%;
+    display:flex;
+    flex-direction:column;
+`;
+const CTALogoOne=styled.img`
+    margin-border:12px;
+    max-width:600px;
+    min-height:1px;
+    display:block;
+    width:100%;
+`;
 export default Login;
