db54a5b2123eecd0121bf83c0ecf83883ce0791f
HarshitChauhan <harshitkumar2503@gmail.com>
2021-04-23 15:22:13 +0530

    Added SignIn functionality with redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,45 @@
 import styled from 'styled-components';
+import {useDispatch, useSelector} from 'react-redux';
+import {useHistory} from 'react-router-dom';
 import {auth, provider} from "../firebase";
+import { selectUserName, selectUserPhoto, setUserLoginDetails } from '../features/users/userSlice';
 
 const Header =(props)=>{
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const userName= useSelector(selectUserName);
+    //const userEmail= useSelector(selectUserEmail);
+    const userPhoto= useSelector(selectUserPhoto);
 
     const handleAuth=()=>{
         auth.signInWithPopup(provider).then((result)=>{
-            console.log(result);
+            setUser(result.user);
         }).catch((error)=>{
             alert(error.message);
         })
     };
+
+    const setUser=(user)=>{
+        dispatch(
+            setUserLoginDetails({
+                name:user.displayName,
+                email:user.email,
+                photo:user.photoURL,
+            })
+        );
+    }
+
     return (
     <Nav>
         <Logo>
             <img src="/images/logo.svg" alt="logo"/>
         </Logo>
-        <NavMenu>
+
+        {!userName 
+        ? (<Login onClick={handleAuth}>Login</Login>)
+        : (
+          <>
+            <NavMenu>
             <a href='/home'>
                 <img src="/images/home-icon.svg" alt="HOME"/>
                 <span>HOME</span>
@@ -41,7 +65,9 @@ const Header =(props)=>{
               <span>SERIES</span>
             </a>
         </NavMenu>
-        <Login onClick={handleAuth}>Login</Login>
+        <UserImg src={userPhoto} alt={userName}/>
+        </>)}
+        
     </Nav>
     );
 }
@@ -150,5 +176,7 @@ const Login=styled.a`
         border-color:transparent;
     }
 `;
-
+const UserImg=styled.img`
+    height:100%;
+`;
 export default Header;
